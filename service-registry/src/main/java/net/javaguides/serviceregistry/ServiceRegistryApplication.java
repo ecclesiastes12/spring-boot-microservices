896fc34ee325b0d8f91b33104f5e39a704f84dbb
ecclesiates12 <solomonotoo74@gmail.com>
2024-07-11 13:27:51 +0000

    service registry using spring cloud eureka service

---
+++ service-registry/src/main/java/net/javaguides/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,21 @@
+package net.javaguides.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+ * service registry
+ * - auto configure the host name and port number for micro-services
+ * - auto service registry and discovery mechanism
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}

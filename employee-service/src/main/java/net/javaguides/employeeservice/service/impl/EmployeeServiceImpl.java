4cec568d31f7d9d65565a53920c2ea1f7eb4a73f
ecclesiates12 <solomonotoo74@gmail.com>
2024-08-12 12:09:36 +0000

    retry with resilience4j configured and implemented

--- employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,14 @@
 package net.javaguides.employeeservice.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import net.javaguides.employeeservice.dto.APIResponseDto;
 import net.javaguides.employeeservice.dto.DepartmentDto;
@@ -20,11 +23,17 @@ import net.javaguides.employeeservice.service.EmployeeService;
 
 /*
  *Code refactor with WebClient and circuit breaker annotation added to getEmployeeById method
+ *
+ *Resilience4j is a library that provides fault tolerance features such as retry, circuit breaker, rate limiter, time limiter, and bulkhead. 
+ *Retry is a feature that automatically retries a failed call using a configurable policy. Thus automatically retry a service when the service
+ *is down or fails
  */
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService{
+	//logger
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 	
 	private EmployeeRepository employeeRepository;
 	
@@ -89,16 +98,21 @@ public class EmployeeServiceImpl implements EmployeeService{
 //	}
 	
 	//NB for learning purpose the exception part is removed and the return type change from EmployeeDto to APIResponseDto
-	
+	// @CircuitBreaker annotation commented out. check EmployeeServiceImpl6.java for circuit breaker annotation and its implentation
+	// Using retry annotation to a method(it is calling to external services)
 	@Override
-	@CircuitBreaker(name = "${spring.application.name}", //application name used as a circuit breaker name
-	fallbackMethod = "getDefaultDeparment") 
+	//@CircuitBreaker(name = "${spring.application.name}", //application name used as a circuit breaker name
+	//fallbackMethod = "getDefaultDepartment") 
+	@Retry(name = "{spring.application.name}", fallbackMethod = "getDefaultDepartment")
 	public APIResponseDto getEmployeeById(Long employeeId) {
-		// TODO Auto-generated method stub
 //		Employee employee = employeeRepository.findById(employeeId).orElseThrow(
 //				//throws exception if user id is not found
 //				() -> new ResourceNotFoundException("Employee", "id", employeeId)
 //				);
+		
+		
+		//log instance
+		LOGGER.info("inside getEmployeeById() method");
 		
 		Employee employee = employeeRepository.findById(employeeId).get();
 		
@@ -139,6 +153,8 @@ public class EmployeeServiceImpl implements EmployeeService{
 	//fallback method for circuit breaker. The purpose of this method is to return default department
 	//when request to fetch department fails
 	public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception) {
+		//log instance
+				LOGGER.info("inside getDefaultDepartment() method");
 		Employee employee = employeeRepository.findById(employeeId).get();
 		
 		//dto object for default department

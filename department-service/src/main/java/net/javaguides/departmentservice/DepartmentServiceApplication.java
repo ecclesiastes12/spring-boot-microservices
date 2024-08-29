1ccb4c80b212d75127e1d60201ef08e628ef4f75
ecclesiates12 <solomonotoo74@gmail.com>
2024-08-29 11:12:32 +0000

    defining genearal information about the api

--- department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
@@ -5,6 +5,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Solomon",
+						email = "solomonotoo74@gmail.com",
+						url = "https://www.ecclinks.com"
+				),
+				
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.ecclinks.com"
+				)
+		),
+		
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Doc",
+				url = "https://www.ecclinks.com"
+		)
+		
+)
+
 @SpringBootApplication
 public class DepartmentServiceApplication {
 	
